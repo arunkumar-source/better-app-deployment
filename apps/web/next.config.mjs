3ac794a2acaa569eed58c5af6e880384d5ab707d
arunkumar-source <arunkumar@webzenith.tech>
2026-02-28 16:51:24 +0530

    next deployment 7

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-  // matcher:['/AddWorkKanban/:path*','/Dash/:path*']
+  middleware: "./proxy.ts",
   env: {
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET,
